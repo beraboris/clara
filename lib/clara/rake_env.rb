2041dce9a1a547fb54929a46891d3ae047072256
Boris Bera <bboris@rsoft.ca>
2013-09-30 20:11:32 -0400

    Added rake environment wrapper
    The RakeEnv class inherints from Rake::Application and makes a few changed to allow for the loading of tasks easily.

---
+++ lib/clara/rake_env.rb
@@ -0,0 +1,19 @@
+require 'rake'
+
+module Clara
+  class RakeEnv < Rake::Application
+    def initialize(work_dir)
+      super()
+      @name = 'clara'
+      @original_dir = work_dir
+    end
+
+    # Make our app the singleton application while we load tasks
+    def load_imports
+      old_app = Rake.application
+      Rake.application = self
+      super
+      Rake.application = old_app
+    end
+  end
+end
